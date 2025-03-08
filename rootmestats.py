08f42ec2db5863698d0cb59c78614368781e96ec
xmarano <leo.gregori@epitech.eu>
2025-03-08 21:33:13 +0100

    [ADD] : Flask python script & SVG html

---
+++ rootmestats.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route("/api", methods=["GET"])
+def get_rootme_stats():
+    username = request.args.get("username")
+    if not username:
+        return "Error : Please give a valid username in the URL", 400
+    
+    url = f"https://rootmeapi.vercel.app/api?username={username}"
+
+    datas = requests.get(url).json()
+    for key, value in datas.items():
+        if value == "Unknown":
+            if key == "userName":
+                datas[key] = "Not Found"
+            elif key == "userPercent":
+                datas[key] = "0%"
+            else:
+                datas[key] = "-"
+
+    return render_template("rootmestats.html", data=datas)
+
+if __name__ == "__main__":
+    app.run(debug=True)
